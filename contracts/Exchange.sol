7c79657b95a8fe62472608737ac6d766480593b0
Shaun <wada.shaun@gmail.com>
2022-11-13 14:23:37 -0800

    12. Cancel Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
     uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
     event Deposit(
         address token, 
@@ -32,6 +33,15 @@ contract Exchange {
         uint256 amountGive,
         uint256 timestamp
     );
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
 
     struct _Order {
         uint256 id;
@@ -42,7 +52,6 @@ contract Exchange {
         uint256 amountGive;
         uint256 timestamp;
     }
-
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -94,6 +103,7 @@ contract Exchange {
     ) public {
 
         // Require token balance
+        require(balanceOf (_tokenGive, msg.sender) >= _amountGive);
 
         // Instantiate a new order
         orderCount = orderCount + 1;
@@ -120,5 +130,28 @@ contract Exchange {
 
     }
 
+    function cancelOrder(uint256 _id) public {
+        // Fetch order
+        _Order storage _order = orders[_id];
 
+        // Ensure the caller of the function is the owner of the order
+        require(address(_order.user) == msg.sender);
+
+        // Order must exist
+        require(_order.id == _id);
+
+        // Cancel the order
+        orderCancelled[_id] = true;
+
+        // Emit event
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
+    }
 }
